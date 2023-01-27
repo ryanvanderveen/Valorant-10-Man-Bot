372a653857f8e0c459d40b75af28465933eafde4
Ryan VanderVeen <92502563+ryanvanderveen@users.noreply.github.com>
2023-01-27 17:07:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -94,21 +94,7 @@ async def setcaps(ctx,cap1 : Player, cap2 : Player):
 @bot.command()
 async def new(ctx):
     lobby_channel = next((i for i in ctx.guild.voice_channels if i.name == options['lobby']), None)
-    embed = discord.Embed(title="Valorant 10 Man Bot",
-            description="How many players are playing?")
-    await ctx.send(embed=embed)
-
-    # This will make sure that the response will only be registered if the following
-    # conditions are met:
-    def check(msg):
-        return msg.author == ctx.author and msg.channel == ctx.channel and int(msg.content) in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-    msg = await bot.wait_for("message", check=check)
     
-    if int(msg.content) != len(lobby_channel.members):
-        embed = discord.Embed(title="Valorant 10 Man Bot",
-        description="Please get the right amount of people to join.")
-        await ctx.send(embed=embed)
     players = lobby_channel.members
     embed = await bot.new_game(players)
     await ctx.send(embed=embed)
