d3b21f0c2a025ffc2933ae88f710c8a52441b0af
Ryan VanderVeen <92502563+ryanvanderveen@users.noreply.github.com>
2023-02-18 02:04:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -142,8 +142,7 @@ class Bot(commands.Bot):
         
         if map_to_pick.lower() in self.map_dict.keys() and self.map_dict[map_to_pick.lower()] == True:
             self.map_dict[map_to_pick.lower()] = False
-            self.pick_dict[map_to_pick.lower()] = True
-            embed_string = f"{map_to_pick} has been picked\n\n The remaining maps are\n\n" + await self.get_remaining_map_string() + "\n\n The picked maps are\n\n" + await self.get_picked_map_string()
+            embed_string = f"{map_to_pick} has been picked\n\n The remaining maps are\n\n" + await self.get_remaining_map_string()
             return discord.Embed(title="Valorant 10 Man Bot", description=embed_string)
 
             """if counter[False] == len(self.map_dict.keys()) - 1: # one map remaining
@@ -175,7 +174,7 @@ class Bot(commands.Bot):
             embed_string = ""
 
             if counter[False] == len(self.map_dict.keys()) - 1: # one map remaining
-                embed_string = "The match will be played on {}".format(next((prettify(k) for k in self.map_dict.keys() if self.map_dict[k]), None))
+                embed_string = "The matches will be played on\n" + await self.get_picked_map_string()
             else:
                 embed_string = f"{map_to_ban} has been banned\n\n The remaining maps are\n\n" + await self.get_remaining_map_string()
             return discord.Embed(title="Valorant 10 Man Bot", description=embed_string)
