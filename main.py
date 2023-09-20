f566377c07422ff053d8d04984cff611619bb577
Ryan VanderVeen <92502563+ryanvanderveen@users.noreply.github.com>
2023-09-20 14:40:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from converters import Player
 with open("config.yaml","r") as file:
     options = yaml.safe_load(file)
 blacklist = open("blacklist.txt","r").readlines()
-command = {'pls ', 'PLS ', 'PlS ', 'PLs ', 'pLs ', 'pLS ', 'plS ', 'Pls ', 'please '}
+command = {'pls ', 'PLS ', 'PlS ', 'PLs ', 'pLs ', 'pLS ', 'plS ', 'Pls '}
 bot = Bot(command, options["scheme"], options["maps"], options["picks"], options["sides"], blacklist)
 
 length = '============'
